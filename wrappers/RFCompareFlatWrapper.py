b62766889ac880990aee5b7e0ae29b53e539f69c
steven <steven.costiou@kloum.io>
2018-02-25 21:28:41 +0100

    Files were not commited last commit.

---
+++ wrappers/RFCompareFlatWrapper.py
@@ -0,0 +1,22 @@
+import copy
+from RFFlatWrapper import RFFlatWrapper
+
+
+class RFCompareFlatWrapper(RFFlatWrapper, object):
+    def transform_node(self):
+        # TODO : node transformation here
+        self.original_node.twin.left = self.builder.ast_load(self.original_node.left.temp_name)
+
+        comparators = list()
+
+        for node in self.original_node.comparators:
+            comparators.append(self.builder.ast_load(node.temp_name))
+        # TODO : node transformation here
+        self.original_node.twin.comparators = comparators
+
+    def flatten_children(self):
+        left = self.original_node.left
+        self.flattened_children.extend(left.wrapper.flat_wrap())
+
+        for node in self.original_node.comparators:
+            self.flattened_children.extend(node.wrapper.flat_wrap())
---
+++ wrappers/RFLiteralFlatWrapper.py
@@ -0,0 +1,10 @@
+from RFFlatWrapper import RFFlatWrapper
+
+
+class RFLiteralFlatWrapper(RFFlatWrapper, object):
+    def transform_node(self):
+        assign = self.builder.assign_named_value(self.original_node.temp_name, self.original_node)
+        self.node_transformation.append(self.builder.build_rf_node(assign))
+
+    def should_wrap(self, rf_node):
+        return True
